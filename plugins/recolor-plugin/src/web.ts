7adce0ce8a31560b0c79af4ff4982065851a3a59
Jaakko Peurasaari <jakepsaari@gmail.com>
2026-03-14 10:42:50 +0200

    Broken android

--- plugins/recolor-plugin/src/web.ts
+++ plugins/recolor-plugin/src/web.ts
@@ -10,12 +10,6 @@ export class AIRecolorWeb extends WebPlugin implements AIRecolor {
 
  async response(color: {value: string}): Promise<{value_ string}> {
         console.log("Color hex", color)
-        return color;
-     }
-
-  async recolorImage(options: { path: string; color: string }): Promise<{ recoloredImagePath: string }> {
-         console.log('WEB recolor fallback', options);
-         return { recoloredImagePath: options.path }; // just pass back path in web
      }
 
  /* This will use some sort own method, to connect to mobile service
